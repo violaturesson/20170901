1e8d58c769c6ead0ecb2595989fb3e81fa865c8c
unknown <violaturesson@gmail.com>
2017-09-01 13:57:02 +0200

    Löst

---
+++ uppgiftA/uppgiftA/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgiftA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in en mening.");
+            string s = Console.ReadLine();
+
+            string[] words = s.Split(' ');
+
+            Console.WriteLine("Antal ord är: "+ words.Length);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.Write(words[i]);
+                Console.WriteLine("("+words[i].Length+")");
+            }
+        }
+    }
+}
---
+++ uppgiftB/uppgiftB/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgiftB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in en mening");
+            string sentence = Console.ReadLine();
+            int j = 0;
+
+            string[] words = sentence.Split(' ');
+
+            Array.Sort(words, (x, y) => y.Length.CompareTo(x.Length));
+            foreach (string s in words)
+            {
+
+                Console.WriteLine(words[j]);
+                j++;
+            }
+
+            j = 0;
+            Array.Sort(words, (x, y) => x.Length.CompareTo(y.Length));
+            foreach (string s in words)
+            {
+
+                Console.WriteLine(words[j]);
+                j++;
+            }
+        }
+    }
+}
