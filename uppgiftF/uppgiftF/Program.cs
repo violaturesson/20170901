c67af72a67337c6f08633fc141d097a8096d4fb1
unknown <violaturesson@gmail.com>
2017-09-01 14:54:14 +0200

    add upgF

---
+++ uppgiftF/uppgiftF/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgiftF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gissa ett tal mellan 1-21");
+            int tal;
+
+            Random slumptal = new Random();
+            int tal1 = slumptal.Next(1, 22);
+
+            bool fel = true;
+            do
+            {
+                tal = Convert.ToInt32(Console.ReadLine());
+
+                if (tal1 == tal)
+                {
+                    Console.WriteLine("Grattis!");
+                    fel = false;
+                }
+            }
+            while (fel);
+            
+
+        }
+    }
+}
